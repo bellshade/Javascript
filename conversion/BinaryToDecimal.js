f699219b9982fed3d4fd770787bbf99d8824ced0
slowy07 <slowy.arfy@gmail.com>
2021-08-15 09:00:00 +0700

    add: biner ke desimal

---
+++ conversion/BinaryToDecimal.js
@@ -0,0 +1,14 @@
+const BinaryToDecimal = (binaryString) => {
+    let decimalNumber = 0;
+    const BinaryDigits = binaryString.split('').reverse();
+    BinaryDigits.forEach((BinaryDigits, index) => {
+        decimalNumber += BinaryDigits * (Math.pow(2, index));
+    })
+    console.log(`decimal of ${binaryString} is ${decimalNumber}`)
+    return decimalNumber;
+}
+
+(() => {
+    BinaryToDecimal('1111111001')
+    BinaryToDecimal('101010101')
+})();
