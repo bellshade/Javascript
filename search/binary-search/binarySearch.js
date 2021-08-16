18d11d3fa97841c3f177e3d9a2a6ad25e2e95b40
wanrabbae <wancode.msg@gmail.com>
2021-08-16 12:24:05 +0700

    add: search algorithm (binary search & linear search)

---
+++ search/binary-search/binarySearch.js
@@ -0,0 +1,20 @@
+// Implementasi Binary Search dalam JavaScript
+// Language: javascript
+// Path: search/binary-search/binarySearch.js
+
+
+function binarySearch(arr, target, low, high) {
+    if (low > high) {
+        return -1;
+    }
+    var mid = Math.floor((low + high) / 2);
+    if (arr[mid] === target) {
+        return mid;
+    }
+    if (arr[mid] > target) {
+        return binarySearch(arr, target, low, mid - 1);
+    }
+    return binarySearch(arr, target, mid + 1, high);
+}
+
+console.log(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 5, 0, 10));
---
+++ search/linear-search/linearSearch.js
@@ -0,0 +1,13 @@
+// implementasi linear search dalam javascript
+// Language: javascript
+// Path: search/linear-search/linearSearch.js
+
+function linearSearch(arr, item) {
+    for (var i = 1; i < arr.length; i++) {
+        if (arr[i] === item) {
+            return i;
+        }
+    }
+}
+
+console.log(linearSearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 7));
