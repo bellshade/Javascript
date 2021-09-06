29820e134ad3dbaf7a5b46302f70d9cdd9285a3f
amdev142 <hirarkyid@gmail.com>
2021-09-06 21:59:51 +0700

    fix: update eslint format code

--- data_structure/filterDuplicateArray.js
+++ data_structure/filterDuplicateArray.js
@@ -7,7 +7,7 @@ let countries = [
 ];
 
 countries = countries.filter(
-  (value, index, arr) => arr.indexOf(value) == index
+  (value, index, arr) => arr.indexOf(value) === index
 );
 
 console.log(countries); // ['Indonesia', 'Malaysia, 'Phillipine, 'Cambodia']
