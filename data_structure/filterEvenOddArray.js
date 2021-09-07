6bd13b8b06ae204c1ca24f340c13fe3c1f80744c
AM Dev <amdev142@gmail.com>
2021-09-07 22:04:42 +0700

    Menambahkan filter genap ganjil pada Array (#7)
    * add: menambahkan algoritma filter duplikasi array
    
    * fix: update eslint format code
    
    * add: menambahkan algoritma filter bilangan genap & ganjil
    
    * fix: menambahkan note
    
    Co-authored-by: amdev142 <hirarkyid@gmail.com>

---
+++ data_structure/filterEvenOddArray.js
@@ -0,0 +1,13 @@
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 20];
+
+let genap = arr.filter((a) => a % 2 === 0);
+let ganjil = arr.filter((a) => a % 2 !== 0);
+
+console.log(genap); // [2, 4, 6, 8, 20];
+console.log(ganjil); // [ 1, 3, 5, 7, 9 ]
+
+/**
+ * Note:
+ * Bilangan Genap: Bilangan yang memiliki sisa jika di bagi 2
+ * Bilangan Ganjil: Bilangan yang tidak memiliki sisa jika di bagi 2
+ **/
