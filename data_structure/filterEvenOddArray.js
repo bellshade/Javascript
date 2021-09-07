f952127a0594101f03a792befae6dd679a4e7a2c
amdev142 <hirarkyid@gmail.com>
2021-09-07 21:14:47 +0700

    fix: menambahkan note

--- data_structure/filterEvenOddArray.js
+++ data_structure/filterEvenOddArray.js
@@ -5,3 +5,9 @@ let ganjil = arr.filter((a) => a % 2 !== 0);
 
 console.log(genap); // [2, 4, 6, 8, 20];
 console.log(ganjil); // [ 1, 3, 5, 7, 9 ]
+
+/**
+ * Note:
+ * Bilangan Genap: Bilangan yang memiliki sisa jika di bagi 2
+ * Bilangan Ganjil: Bilangan yang tidak memiliki sisa jika di bagi 2
+ **/
