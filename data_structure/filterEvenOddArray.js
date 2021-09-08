706f5cd122dbd5ba13180997ca8a8bb8f70af758
amdev142 <hirarkyid@gmail.com>
2021-09-08 08:54:03 +0700

    fix: update description code

--- data_structure/filterEvenOddArray.js
+++ data_structure/filterEvenOddArray.js
@@ -7,6 +7,12 @@ console.log(genap); // [2, 4, 6, 8, 20];
 console.log(ganjil); // [ 1, 3, 5, 7, 9 ]
 
 /**
+ * Penjelasan code:
+ * Variable genap: Dilakukan filterisasi tiap-tiap index pada sebuah array menggunakan operator modulus, jika index sisa bagi 2 bernilai 0 / tidak memiliki sisa value maka dia bilangan Genap.
+ * Varibale ganjil: Kebalikan dari penjelasan di atas akan tetapi jika hasil dari sisa bagi 2 bernilai bukan 0 / memiliki sisa value maka dia bilangan Ganjil.
+ **/
+
+/**
  * Note:
  * Bilangan Genap: Bilangan yang memiliki sisa jika di bagi 2
  * Bilangan Ganjil: Bilangan yang tidak memiliki sisa jika di bagi 2
