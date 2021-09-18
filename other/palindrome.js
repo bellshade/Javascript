914d075880f776e6462daaadd0cda04e27760a41
toufiqnuur <taufik2017yk@gmail.com>
2021-09-18 17:16:23 +0700

    fix: menyederhanakan code

--- other/palindrome.js
+++ other/palindrome.js
@@ -2,18 +2,7 @@
    Palindrom adalah sebuah kata, frasa, angka maupun susunan lainnya yang dapat dibaca dengan sama baik dari depan maupun belakang.
 */
 function isPalindrome(string) {
-  string = string.toLowerCase();
-
-  for (let i = 0; i < Math.floor(string.length / 2); i++) {
-    let indexAwal = i;
-    let indexAkhir = string.length - i - 1;
-
-    if (string[indexAwal] !== string[indexAkhir]) {
-      return false;
-    }
-  }
-
-  return true;
+  return string.toLowerCase() === [...string.toLowerCase()].reverse().join("");
 }
 
 console.log(isPalindrome("Frog"));
