99daf88613d33df6e169e3e53793e4b43f98d784
Fikri ihsan <mfikriihsan03@gmail.com>
2021-09-28 17:50:00 +0700

    docs : menambahkan penjelasan dan memperbaiki strukturnya

--- basic/conditioning/if_else_elseIf.js
+++ basic/conditioning/if_else_elseIf.js
@@ -1,27 +1,21 @@
 // * macam-macam cara pengkodisian
 
+//--------if-------------------
 // di dalam javascript ada sebuah cara 
 // dimana kita bisa mengeksekusi sebuah kode, jika syarat yang kita tentukan sudah terpenuhi (true)
 // yakni dengan menggunakan statement if
 // biasanya syarat di if statement menggunakan comparisonOperator ataupun logilalOperator
+//  kode yang didalam if statement akan dieksekusi hanya jika syarat mengeluarkan nilai true
 
-// contoh
-if(syarat) {
-    //  kode yang didalam if statement akan dieksekusi hanya jika syarat mengeluarkan nilai true
-}
 
-// nah, javascript juga memiliki sebuah cara
-// untuk mengeksekusi sebuah kode, jika syarat yang kita tentukan tidak terpenuhi (false)
+//------------else---------------------
+// javascript juga memiliki sebuah cara
+// untuk mengeksekusi sebuah kode jika syarat yang kita tentukan tidak terpenuhi (false)
 // yakni dengan menggunakan statement else
+// jika kode didalam if tidak dieksekusi karena syarat tidak terpenuhi atau mengeluarkan nilai false,
+// maka kode didalam blok else akan dieksekusi
 
-//contoh
-//kita akan menggunakan if sebagai pengecek syarat
-if(syarat){
-    //  kode yang didalam blok if statement akan dieksekusi hanya jika syarat mengeluarkan nilai true
-}else{
-    // jika kode didalam if tidak dieksekusi karena syarat tidak terpenuhi atau mengeluarkan nilai false,
-    // maka kode didalam blok else akan dieksekusi
-}
+
 
 
 // mari kita lihat contoh implementasi nya
@@ -34,22 +28,16 @@ if( x < 11){
     // jika tidak
     console.log("SALAH, variabel x lebih besar dari angka 11 ")
 }
-// apakah sudah bisa dimengerti ?
 
+
+//--------------else if-------------
 // nah, adalagi nih sebuah cara di javascript
 // untuk melakukan pengecekan ber-urut
 // yakni dengan else if
+// jika blok if pertama bernilai false, maka kode akan melakukan pengecekan dengan syaratkedua di blok else if
+// jika syarat kedua bernilai true, maka blok else if akan dijalankan
 
-// contoh
-if(syarat){
-    //  kode yang didalam blok if statement akan dieksekusi hanya jika syarat mengeluarkan nilai true
-}else if(syaratkedua){
-    // jika blok if pertama bernilai falsse, maka kode akan melakukan pengecekan dengan syaratkedua di blok else if
-    // jika syarat kedua bernilai true, maka blok else if akan dijalankan
-}else{
-    // jika kode didalam if dan else if tidak dieksekusi karena syarat tidak terpenuhi atau mengeluarkan nilai false,
-    // maka kode didalam blok else akan dieksekusi
-}
+
 
 // mari kita ke contoh implementasi nya
 let jam = 13
