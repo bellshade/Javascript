c9df7a7ae4ec1b172c1c2349cb2249138ba1186f
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2021-09-28 11:35:53 +0700

    add: menambahkan penjelasan string dan cara memanipulasinya

---
+++ basic/string_and_string_manipulation/anotherStringDeclaration.js
@@ -0,0 +1,27 @@
+// Hapus beberapa kode yang dikomentari untuk melihat hasilnya.
+// Beberapa kode dikomentari karena prettier
+
+// Jika komentar dihapus, kode ini akan menimbulkan error
+// const errorPertama = """"
+// const errorKedua = ''''
+// const errorKetiga = ````;
+
+// ==== Jenis Deklarasi === //
+const kutipDua = "Ini adalah tulisan";
+console.log(kutipDua);
+
+// const kutipSatu = 'Tulisan ini didalam kutip satu';
+// console.log(kutipSatu);
+
+const templateLitteral = `Saya tinggal di planet bumi.`;
+console.log(templateLitteral);
+
+// === Penggunaan Deklarasi laim dalam string === //
+const kutipSatuDidalamnya = "Nama benda ini adalah 'Sapu'";
+console.log(kutipSatuDidalamnya);
+
+const kutipDuaDidalamnya = 'Didalam kutip satu ini terdapat ""';
+console.log(kutipDuaDidalamnya);
+
+// const kutipDuaDouble = "Kutip dua didalam \"Kutip Dua\""
+// console.log(kutipDuaDouble);
