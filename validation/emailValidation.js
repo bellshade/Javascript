d29019aff950d8e2d50ac41d8d7a4073e672e9e3
amdev142 <hirarkyid@gmail.com>
2021-10-03 15:47:33 +0700

    add: "Menambahkan algoritma email validation"

---
+++ validation/emailValidation.js
@@ -0,0 +1,20 @@
+let string = 'bellshade';
+let mail = 'bellshade@gmail.com';
+let pattern = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
+
+if(string.match(pattern)) {
+  console.log('This is a valid email'); 
+} else if(mail.match(pattern)) {
+  console.log('This is a valid email'); 
+} else {
+  console.log('Invalid email');
+}
+
+/**
+ * Penjelasan kode:
+ * 
+ * Match adalah fungsi yang tersedia pada string untuk melakukan pengecekan menggunakan Regex (Regular Expression)
+ * 
+ * Pada kondisi if pertama akan bernilai false karena yang di cek adalah variable yang bernilai kalimat biasa
+ * Pada kondisi kedua ini akan bernilai true karena yang di cek adalah sebuah email yang valid
+ */
