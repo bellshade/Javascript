c1f7f2d0aa67697be0db6bf7fc803ace76ef03fc
MaxdhaMax <maxdhamaxi@gmail.com>
2021-10-04 14:15:51 +0700

    add: TypeOf

--- basic/002_variable_datatype/typeOf.js
+++ basic/002_variable_datatype/typeOf.js
@@ -1,15 +1,15 @@
 const umur = 17;
-console.log(typeof(umur)) // Number
+console.log(typeof umur); // Number
 
 let namaBuah = "Pepaya";
-console.log(typeof(namaBuah)) // String
+console.log(typeof namaBuah); // String
 
 var hoax = true;
-console.log(typeof(hoax))  // Boolean
+console.log(typeof hoax); // Boolean
 
 var array = [];
-console.log(typeof(array)) // Object
+console.log(typeof array); // Object
 
-// jika suatu nama variabel belum didefinisikan, tapi kita panggil type of nya 
+// jika suatu nama variabel belum didefinisikan, tapi kita panggil type of nya
 //maka hasilnya menjadi undefined
-console.log(typeof(varBaru)) 
+console.log(typeof varBaru);
