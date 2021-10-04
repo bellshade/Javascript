4e9661d4b5bb3454a25716e72c05f80085005f2e
nugrohoff <fakhrl.fanani.nugroho@gmail.com>
2021-10-04 10:56:07 +0700

    docs: dokumentasi di file ipAddressValidation.js

--- validation/ipAddressValidation.js
+++ validation/ipAddressValidation.js
@@ -1,5 +1,5 @@
 const ipAddress = "0.0.0.0";
-const anodtherIpAdress = "127.0.0.1";
+const anotherIpAdress = "127.0.y.x";
 
 const isValidIpAddress = (ipAddress) => {
   const splitted = ipAddress.split(".");
@@ -24,4 +24,24 @@ const isValidIpAddress = (ipAddress) => {
 };
 
 console.log(isValidIpAddress(ipAddress));
-console.log(isValidIpAddress(anodtherIpAdress));
+console.log(isValidIpAddress(anotherIpAdress));
+
+/**
+ * Penjelasan Kode
+ *
+ * Variable ipAddress berisi nilai IP Address yang valid
+ * Variable anotherIpAddress berisi nilai IP Address yang tidak valid
+ *
+ * Fungsi isValidIpAddress() menerima parameter berupa String IP Address
+ * didalam fungsi tersebut terdapat berbagai pengecekan
+ *
+ * 1. Variable splitted berisi parameter ipAddress yang di pecah menjadi array berdasarkan . (dot / titik) menggunakan fungsi split()
+ * untuk dokumentasi split() bisa di buka di https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/split?retiredLocale=id
+ *
+ * 2. Cek apakah setiap okte pada IP Address berupa angka menggunakan perulangan for
+ * 3. Cek jumlah okte pada IP Address, IP Addres yang valid berjumlah 4 okte
+ * 4. Cek apakah setiap okte diantara 0 sampai 255
+ *
+ * Jika semua pengecekan diatas terlewati maka IP Address tersebut valid.
+ *
+ */
