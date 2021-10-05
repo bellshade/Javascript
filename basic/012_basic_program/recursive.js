413fd2b869264e016d4967a6f5ad26141e50c152
Fikriihsan03 <64629006+Fikriihsan03@users.noreply.github.com>
2021-10-05 19:39:49 +0700

    Recursive program (#84)
    * add : menambahkan algoritma recursive program yang baru
    
    * fix : merapikan indentasi dengan prettier

---
+++ basic/012_basic_program/recursive.js
@@ -0,0 +1,22 @@
+// implementasi recursive untuk mencari ujung dari garis geometri
+
+// yang perlu diketahui :
+// a = suku pertama
+// n = banyak suku
+
+// cara kerja :
+
+// setiap pemanggilan function ,kita akan merubah nilai n dari variabel u1, misalnya n = 4, maka u1 = 3
+// kemudian kita melakukan pengecekan dari variabel u1 , jika lebih besar dari 0.
+// maka function getEndOfGeometryLine akan memanggil dirinya sendiri dengan parameter n dan a yang sudah diperbarui
+// kemudian kembali ke langkah pertama tadi hingga u1 sama dengan 0, maka kita akan print un atau hasil akhir dari baris ke n di baris geometri
+const getEndOfGeometryLine = (a, n) => {
+  let un = a;
+  let u1 = n - 1;
+  if (u1 > 0) {
+    getEndOfGeometryLine(un + a, u1);
+  } else {
+    console.log(un);
+  }
+};
+console.log(getEndOfGeometryLine(2.5, 4));
--- cipher/rot13.js
+++ cipher/rot13.js
@@ -5,18 +5,17 @@
 
 // normal function
 function rot13(str) {
-    return str.replace(/[a-zA-Z]/g, function(chr) {
-      var start = chr <= "Z" ? 65 : 97;
-      return String.fromCharCode(start + (chr.charCodeAt(0) - start + 13) % 26);
-    });
-  }
+  return str.replace(/[a-zA-Z]/g, function (chr) {
+    var start = chr <= "Z" ? 65 : 97;
+    return String.fromCharCode(start + ((chr.charCodeAt(0) - start + 13) % 26));
+  });
+}
 
 // onliner
 // versi singkat dari normal function
 // namun penggunaannya kurang disarankan karena code nya menjadi sulit dipahami
 // karena code yang baik tidak hanya code yang tercepat, terpendek, tapi juga code yang mudah dipahami agar gampang di maintenance
 // const rot13 = str => str.replace(/[a-zA-Z]/g,chr=>String.fromCharCode((chr <= "Z"? 65 : 97)+(chr.charCodeAt(0)-(chr <= "Z" ? 65 : 97)+13)%26));
-
 
 console.log(rot13("hello world")); //uryyb jbeyq
 console.log(rot13("uryyb jbeyq")); //hello world
