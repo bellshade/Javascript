01a804700b18a0dda8b170f8bab56df5c9f22484
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2021-10-05 17:33:35 +0700

    fix(UnknownScope): prettier reformat

--- conversion/TimeConversion.js
+++ conversion/TimeConversion.js
@@ -1,13 +1,12 @@
 const timeConversion = (time) => {
-    let AMPM = time.slice(-2);
-    let timeArr = time.slice(0, -2).split(":");
-    if (AMPM === "AM" && timeArr[0] === "12") {
-        timeArr[0] = "00";
-    } 
-    else if (AMPM === "PM") {
-        timeArr[0] = (timeArr[0] % 12) + 12;
-    }  
-    return timeArr.join(":");
+  let AMPM = time.slice(-2);
+  let timeArr = time.slice(0, -2).split(":");
+  if (AMPM === "AM" && timeArr[0] === "12") {
+    timeArr[0] = "00";
+  } else if (AMPM === "PM") {
+    timeArr[0] = (timeArr[0] % 12) + 12;
+  }
+  return timeArr.join(":");
 };
 console.log(timeConversion("11:00:59PM"));
 
--- validation/emailValidation.js
+++ validation/emailValidation.js
@@ -2,19 +2,19 @@ const string = "bellshade";
 const mail = "bellshade@gmail.com";
 const pattern = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
 
-if(string.match(pattern)) {
-  console.log("This is a valid email"); 
-} else if(mail.match(pattern)) {
-  console.log("This is a valid email"); 
+if (string.match(pattern)) {
+  console.log("This is a valid email");
+} else if (mail.match(pattern)) {
+  console.log("This is a valid email");
 } else {
   console.log("Invalid email");
 }
 
 /**
  * Penjelasan kode:
- * 
+ *
  * Match adalah fungsi yang tersedia pada string untuk melakukan pengecekan menggunakan Regex (Regular Expression)
- * 
+ *
  * Pada kondisi if pertama akan bernilai false karena yang di cek adalah variable yang bernilai kalimat biasa
  * Pada kondisi kedua ini akan bernilai true karena yang di cek adalah sebuah email yang valid
  */
