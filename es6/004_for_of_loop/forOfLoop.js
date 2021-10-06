dc4b1531f9b566e913ea80fb9302638e6bfb9ae2
Gunawan Nur Ahmad <54525132+GunawanAhmad@users.noreply.github.com>
2021-10-06 19:16:08 +0700

    docs: menambahkan dokumentasi for of loop pada es6

---
+++ es6/004_for_of_loop/forOfLoop.js
@@ -0,0 +1,10 @@
+// deklarasi array
+let array = ["Deku", "Bakugo", "Todoroki", "Uraraka"];
+
+/* 
+item adalah variable yang digunakan sebagai represenetasi
+tiap value pada array.
+*/
+for (let item of array) {
+  console.log(value); // Deku, Bakugo, Todoroki, Uraraka
+}
