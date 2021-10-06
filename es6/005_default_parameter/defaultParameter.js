0dc9dc79597928fd3e9f85f19b84800d1aa3795b
Rizky Ikhwan <79355239+rizkyikhwan@users.noreply.github.com>
2021-10-06 19:53:26 +0700

    Update defaultParameter.js

--- es6/005_default_parameter/defaultParameter.js
+++ es6/005_default_parameter/defaultParameter.js
@@ -7,14 +7,14 @@ function jualBuah(buah, berat, harga) {
 console.log(jualBuah("semangka", "1 kg", "22 ribu")); // Output : Buah semangka dengan berat 1 kg, di pasar seharga 22 ribu.
 
 // 2. Tidak memberikan argument pada salah satu dan semua parameter
-console.log(jualBuah("semangka", "1 kg")); // Output : Buah semangka dengan berat 1 kg, di pasar seharga undifined.
-console.log(jualBuah()); // Output : Buah undifiend dengan berat undifiend, di pasar seharga undifined.
+console.log(jualBuah("semangka", "1 kg")); // Output : Buah semangka dengan berat 1 kg, di pasar seharga undefined.
+console.log(jualBuah()); // Output : Buah undefiend dengan berat undefiend, di pasar seharga undefined.
 
 /* - Pada fungsi jualBuah memiliki parameter (buah, berat, harga).
    Artinya saat kita memanggil sebuah fungsi jualBuah, maka parameter di dalam fungsi tersebut akan menangkap sebuah argument yang kita berikan seperti contoh nomor 1, yaitu "semangka", "1 kg", "22 ribu".
  
 * - Bagaimana jika argument tersebut tidak kita isi?
-  Apabila kita tidak memberikan argument pada sebuah parameter, maka argument tersebut akan memberikan nilai default, yaitu "undifiend".  
+  Apabila kita tidak memberikan argument pada sebuah parameter, maka argument tersebut akan memberikan nilai default, yaitu "undefiend".  
   
  */
 
