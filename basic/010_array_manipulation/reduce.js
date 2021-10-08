8c0baf037200c04034dfb048307a31c474d1ca6d
unknown <doni.yukri@gmail.com>
2021-10-08 22:28:29 +0700

    feat: menambahkan reduce pada manipulasi array

--- basic/010_array_manipulation/reduce.js
+++ basic/010_array_manipulation/reduce.js
@@ -3,7 +3,7 @@ const pemasukan = 1000000;
 const pengeluaran = [250000, 80000, 140000, 310000];
 
 const sisaPemasukan = pengeluaran.reduce(function(acc, curr){
-  return acc - curr
+  return acc - curr;
 }, pemasukan);
 // nilai variabel sisaPemasukan diambil dari hasil reduce array pengeluaran
 // acc adalah akumulator (accumulator)
