8521ec607a07e7316e11075fb72d62faa40b245f
unknown <doni.yukri@gmail.com>
2021-10-08 16:55:27 +0700

    add: menambah strict mode pada basic

---
+++ basic/013_strict_mode/strictMode.js
@@ -0,0 +1,12 @@
+// ==strict mode==
+"use strict"; // comment bagian ini dan baris 10, lalu uncomment baris 8 untuk melihat kode tanpa strict mode
+
+// strict mode sebaiknya ditulis dibagian paling atas, diatasnya boleh ada komentar
+
+let pekerjaan = "front end developer";
+
+// perkerjaan = "fullstack developer";   // typo pada kata pekerjaan menjadi perkerjaan
+
+pekerjaan = "fullstack developer";
+
+console.log(pekerjaan);
