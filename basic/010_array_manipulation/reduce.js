82197d759b04c2775b0d43eca6815f16b7e95131
Syzkii <rifkiardianakbar.raa@gmail.com>
2021-10-10 03:49:07 +0700

    docs: Menambahkan 016_boolean pada basic javascript

--- basic/010_array_manipulation/reduce.js
+++ basic/010_array_manipulation/reduce.js
@@ -2,7 +2,7 @@
 const pemasukan = 1000000;
 const pengeluaran = [250000, 80000, 140000, 310000];
 
-const sisaPemasukan = pengeluaran.reduce(function(acc, curr){
+const sisaPemasukan = pengeluaran.reduce(function (acc, curr) {
   return acc - curr;
 }, pemasukan);
 // nilai variabel sisaPemasukan diambil dari hasil reduce array pengeluaran
