df6535582bb0eb816b0479c0cd81d486fff56e44
raifandi <92188764+KrOxYs@users.noreply.github.com>
2021-10-10 11:37:23 +0700

    Update fizzBuzz.js

--- other/fizzBuzz.js
+++ other/fizzBuzz.js
@@ -11,11 +11,11 @@
 // Kita akan membuat sampai 100 (optional)
  for(let i = 0; i <= 100; i++ ) {
      if(i % 3 === 0 && i % 5 === 0) {
-         console.log('FizzBuzz');
+         console.log("FizzBuzz");
      } else if (i % 3 === 0) {
-         console.log('Fizz');
+         console.log("Fizz");
      } else if(i % 5 === 0 ) {
-         console.log('Buzz');
+         console.log("Buzz");
      } else {
          console.log(i);
      }
