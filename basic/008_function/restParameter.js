9ad324b971b5ccfe550f600b1fb205aae9a4ced3
KrOxYs <raifandibaiqi@gmail.com>
2021-10-12 17:33:04 +0700

    feat: menambahkan rest parameter

---
+++ basic/008_function/restParameter.js
@@ -0,0 +1,21 @@
+// Apa sih itu rest parameter 
+// Rest parameter adalah sebuah fungsi di javascript yang memungkinkan bisa value parameter
+
+// Langsung ke codenya 
+
+// kita akan membuat funcion yang bernama sum dengan nama parameternya name dan nilai  
+function sum (name,...nilai ) {
+    // ...data merupakan rest parameter yang akan menyimpan value banyak 
+    let total = 0;
+    // kita mambuat variable yang bernama total
+    // Lalu membuat perulangan for of 
+    for(const value of nilai) {
+        total += value;
+    }
+    console.log(`total nilai ${name} adalah  ${total}`);
+}
+
+sum(`rai`,10,20,30,40,50);
+
+// Output 
+// total nilai rai adalah 150 
