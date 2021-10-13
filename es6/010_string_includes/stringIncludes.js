e034b7120a5e5e590d7321477bbf1c07a912f45c
KrOxYs <raifandibaiqi@gmail.com>
2021-10-13 16:53:51 +0700

    fix: memperbaiki algoritma yang sudah ada

---
+++ es6/010_string_includes/stringIncludes.js
@@ -0,0 +1,16 @@
+// Contoh string/kata
+const nama_kamu = "Hello my name is rai";
+nama_kamu.includes("nama");
+// Output
+// true
+
+let value = [2,9,6,10,4];
+value.includes(2);
+// Output
+// true
+
+let num = [3,8,1,6,11];
+num.includes(0);
+// Outpun
+// false
+
