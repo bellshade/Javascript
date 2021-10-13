beb60207709ea56b8546236dd98b28e89df3935b
Wahyu Maulana <wahyaumau@gmail.com>
2021-10-13 15:54:38 +0700

    feat: Menambahkan method-method untuk operasi binary tree

--- data_structure/binaryTree.js
+++ data_structure/binaryTree.js
@@ -117,6 +117,113 @@ class BinaryTreeUtil {
     }
     return data;
   }
+
+  /**
+   * Static method untuk mencari data pada binary tree, dengan nilai pencarian yang sudah ditentukan.
+   * Mengembalikan data pada binary tree berdasarkan nilai pencarian jika ditemukan, atau null.
+   * Pencarian dilakukan dengan membandingkan data setiap node pada binary tree dengan nilai pencarian.
+   * Pencarian dilakukan secara rekursif untuk setiap node pada binary tree.
+   * @example
+   * root
+   *     9
+   *    / \
+   *   1   8
+   *  / \
+   * 2   7
+   * // returns 8
+   * BinaryTreeUtil.search(root, 8);
+   * @example
+   * // returns null
+   * BinaryTreeUtil.search(root, 10);
+   * @param {TreeNode} root - Node binary tree yang akan dikenakan pencarian.
+   * @param {*} data - Target pencarian.
+   * @returns {*|null} - Mengembalikan data pada node berdasarkan pencarian jika ditemukan, atau null.
+   */
+  static search(root, data) {
+    if (root == null) return null;
+    if (root.data === data) {
+      return root.data;
+    }
+    const dataLeftChild = this.search(root.left, data);
+    if (dataLeftChild) return dataLeftChild;
+    return this.search(root.right, data);
+  }
+
+  /**
+   * Static method untuk menghitung jumlah node pada binary tree secara rekursif.
+   * @example
+   * root
+   *      3
+   *    /   \
+   *   5     6
+   *  / \   /
+   * 2   7 1
+   * // returns 6
+   * BinaryTreeUtil.count(root);
+   * @param {TreeNode} root - Node binary tree yang akan dihitung jumlah node nya.
+   * @returns {Number} - Mengembalikan jumlah node pada binary tree.
+   */
+  static count(root) {
+    if (root == null) return 0;
+    return 1 + this.count(root.left) + this.count(root.right);
+  }
+
+  /**
+   * Static method untuk mengambil list leaf dari binary tree.
+   * Leaf dari binary tree adalah node dari binary tree yang tidak mempunyai children.
+   * @example
+   * root
+   *      9
+   *    /   \
+   *   8     6
+   *  / \   /
+   * 3   7 1
+   * // returns node 3, node 7, node 1.
+   * BinaryTreeUtil.getLeaves(root);
+   * @param {TreeNode} root - Node binary tree yang akan dicari leaf nya.
+   * @returns {TreeNode[]} - Mengembalikan list leaf dari binary tree dalam bentuk array.
+   */
+  static getLeaves(root) {
+    const leaveNodes = [];
+    if (root != null) {
+      if (!root.left && !root.right) {
+        leaveNodes.push(root);
+      }
+      leaveNodes.push(...this.getLeaves(root.left));
+      leaveNodes.push(...this.getLeaves(root.right));
+    }
+    return leaveNodes;
+  }
+
+  /**
+   * Static method untuk mengambil list subtree dari binary tree.
+   * Subtree dari binary tree adalah semua keturunan dari root pada binary tree.
+   * @example
+   * root
+   *      9
+   *    /   \
+   *   8     6
+   *  / \   /
+   * 3   7 1
+   * // returns node 8, node 3, node 7, node 6, node 1.
+   * BinaryTreeUtil.getSubTrees(root);
+   * @param {TreeNode} root - Node binary tree yang akan dicari subtree nya.
+   * @returns {TreeNode[]} - Mengembalikan list subtree dari binary tree dalam bentuk array.
+   */
+  static getSubTrees(root) {
+    const subTrees = [];
+    if (root != null) {
+      if (root.left) {
+        subTrees.push(root.left);
+      }
+      if (root.right) {
+        subTrees.push(root.right);
+      }
+      subTrees.push(...this.getSubTrees(root.left));
+      subTrees.push(...this.getSubTrees(root.right));
+    }
+    return subTrees;
+  }
 }
 
 // Membuat binary tree sebagai berikut:
@@ -149,3 +256,58 @@ newRoot.insert(17);
 console.log(BinaryTreeUtil.inOrder(newRoot)); // [17, 2, 4, 10]
 console.log(BinaryTreeUtil.preOrder(newRoot)); // [4, 2, 17, 10]
 console.log(BinaryTreeUtil.postOrder(newRoot)); // [17, 2, 10, 4]
+
+// Membuat binary tree sebagai berikut:
+//       7
+//      / \
+//     1   4
+//    /
+//   9
+const bTree = new TreeNode(7);
+bTree.insert(1);
+bTree.insert(4);
+bTree.insert(9);
+console.log(BinaryTreeUtil.search(bTree, 7)); // 7
+console.log(BinaryTreeUtil.search(bTree, 2)); // null
+console.log(BinaryTreeUtil.count(bTree)); // 4
+console.log(BinaryTreeUtil.getLeaves(bTree).map((treeNode) => treeNode.data)); // [9, 4]
+console.log(BinaryTreeUtil.getSubTrees(bTree).map((treeNode) => treeNode.data)); // [1, 4, 9]
+
+// Membuat binary tree sebagai berikut:
+//        4
+//      /   \
+//     1     9
+//    / \   / \
+//   8   3 7   6
+//  / \
+// 2   5
+
+const bTreeTest = new TreeNode(4);
+bTreeTest.insert(1);
+bTreeTest.insert(9);
+bTreeTest.insert(8);
+bTreeTest.insert(3);
+bTreeTest.insert(7);
+bTreeTest.insert(6);
+bTreeTest.insert(2);
+bTreeTest.insert(5);
+// > BinaryTreeUtil.search(bTreeTest, 9);
+// 9
+// > BinaryTreeUtil.search(bTreeTest, 5);
+// 5
+// > BinaryTreeUtil.search(bTreeTest, 7);
+// 7
+// > BinaryTreeUtil.search(bTreeTest, 6);
+// 6
+// > BinaryTreeUtil.search(bTreeTest, 10);
+// null
+// > BinaryTreeUtil.count(bTreeTest);
+// 9
+// > BinaryTreeUtil.getLeaves(bTreeTest).length;
+// 5
+// > BinaryTreeUtil.getLeaves(bTreeTest).map((nodeTree) => nodeTree.data);
+// [2, 5, 3, 7, 6]
+// > BinaryTreeUtil.getSubTrees(bTreeTest).length;
+// 8
+// > BinaryTreeUtil.getSubTrees(bTreeTest).map((nodeTree) => nodeTree.data);
+// [1, 9, 8, 3, 2, 5, 7, 6]
