36110c8e4f8772984aa8b263800b13a60e3316cf
Syzkii <59856287+syzkii@users.noreply.github.com>
2021-10-13 17:51:44 +0700

    fix: Mengubah dokumen pada basic (#133)
    * feat: Menambah algoritma baru di basic
    
    * fix: Mengubah dokumen pada basic

---
+++ basic/016_number/number.js
@@ -0,0 +1,49 @@
+//Tipe Data Number
+//start
+let x = 10;
+console.log(typeof x);
+
+/* output: number */
+
+let y = 17.25;
+console.log(typeof y);
+
+/* output: number */
+//end
+
+//start
+let a = 12;
+let b = 9;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+
+/* output:
+    21
+    3
+    108
+    1.3333333333333333
+    3
+    */
+//end
+
+//start
+/* Increment dan Decrement */
+
+let postfix = 5;
+console.log(postfix++);
+/* output: 5 */
+
+let prefix = 5;
+console.log(++prefix);
+/* output: 6 */
+//end
+
+/*Pada operator aritmatika juga terdapat operator increment (++) dan decrement (--). Operator increment dan decrement digunakan untuk menambahkan atau mengurangi nilai 1 pada nilai variabel yang ada sekarang.
+  Operator ini dapat dituliskan sebelum atau sesudah variabel, tetapi hal tersebut bukan berarti sama. Berikut ketentuannya:
+  Jika dituliskan setelah variabel (x++), statement akan menghasilkan nilai variabel sebelum ditingkatkan nilainya.
+  Jika dituliskan sebelum variabel (++x), statement akan menghasilkan nilai variabel setelah ditingkatkan nilainya.
+  */
--- basic/README.md
+++ basic/README.md
@@ -14,5 +14,6 @@
 12. [Manipulasi Object](012_object_manipulation/)
 13. [Class](013_classes)
 14. [Strict Mode](014_strict_mode)
-15. Folder berisi contoh program sederhana dengan menggunakan fungsi, rekayasa string, logika
-16. [Boolean](016_boolean/)
+15. [Boolean](015_boolean/)
+16. [Number](016_number/)
+17. Folder berisi contoh program sederhana dengan menggunakan fungsi, rekayasa string, logika
