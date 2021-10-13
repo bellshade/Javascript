68f4d246cb631bec337c03bb1de238bdd3477d6b
suandicreative <90173857+suandicreative@users.noreply.github.com>
2021-10-13 12:40:56 +0700

    fix: support leap year (tahun kabisat)

--- conversion/dateToDay.js
+++ conversion/dateToDay.js
@@ -41,12 +41,24 @@ const dateToDay = (tanggal) => {
   }
   const th = tahun % 100; // mengambil 2 digit tahun dari belakang
   const abad = Math.floor(tahun / 100); // mengambil 2 digit tahun dari depan
-  const hasil = Math.abs((tgl + Math.floor(2.6 * listBulan[bulan] - 0.2) - 2 * abad + th + Math.floor(th / 4) + Math.floor(abad / 4)) % 7);
 
-  return listHari[hasil];
+  if ((bulan === 2) && (tahun % 4 === 0) && (tahun % 100 !== 0) || (tahun % 400 === 0)) {
+    const hasil = Math.abs((tgl + Math.floor(2.6 * listBulan[bulan] - 0.2) - 2 * abad + th + Math.floor(th / 4) + Math.floor(abad / 4)+5) % 7);
+    return listHari[hasil];
+  } else {
+    const hasil = Math.abs((tgl + Math.floor(2.6 * listBulan[bulan] - 0.2) - 2 * abad + th + Math.floor(th / 4) + Math.floor(abad / 4)) % 7);
+    return listHari[hasil];
+  }
 };
 
 console.log(dateToDay("13/10/2021")); // output: Rabu
 console.log(dateToDay("13/10/2020")); // output: Selasa
 console.log(dateToDay("13/10/2019")); // output: Minggu
 console.log(dateToDay("17/08/1945")); // output: Jumat
+console.log(dateToDay("29/03/2020")); // output: Minggu
+
+// Leap Year Test
+console.log(dateToDay("29/02/2020")); // output: Sabtu
+console.log(dateToDay("29/02/2024")); // output: Kamis
+console.log(dateToDay("29/02/2028")); // output: Selasa
+console.log(dateToDay("29/02/2032")); // output: Minggu
