510a16f85318353a73e6502213c476d787aa3768
raifandi <92188764+KrOxYs@users.noreply.github.com>
2021-10-14 12:27:09 +0700

    fix: memperbaiki algoritma yang sudah ada
    sudah di ganti mohon maaf telah merepotkan

--- es6/010_string_includes/stringIncludes.js
+++ es6/010_string_includes/stringIncludes.js
@@ -4,13 +4,4 @@ perkenalan.includes("nama");
 // Output
 // true
 
-let value = [2,9,6,10,4];
-value.includes(2);
-// Output
-// true
-
-let num = [3,8,1,6,11];
-num.includes(0);
-// Output
-// false
 
