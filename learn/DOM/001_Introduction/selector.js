060ccf6e70646987a66ec959bad93878f839dd98
hasheemi-rafsanjani <hasemiandika78@gmail.com>
2021-10-20 16:52:00 +0700

    fix: mengganti class ke id

--- learn/DOM/001_Introduction/selector.js
+++ learn/DOM/001_Introduction/selector.js
@@ -1,17 +1,18 @@
 // cara menyeleksi elemen p adalah dengan cara
 // 1. dengan menggunakan selector document.getElementsByTagName
 document.getElementsByTagName("p");
-// 2. dengan menggunakan selector document.getElementsByClassName
-document.getElementsByClassName("paragraf");
+
+// 2. dengan menggunakan selector document.getElementById
+document.getElementById("paragraf");
 // 3. dengan menggunakan selector document.querySelector
-document.querySelector("p.paragraf");
+document.querySelector("p#paragraf");
 
 // cara menyeleksi elemen span adalah dengan cara
 // 1. dengan menggunakan selector document.getElementsByTagName
 document.getElementsByTagName("span");
-// 2. dengan menggunakan selector document.getElementById
-document.getElementById("kata");
+// 2. dengan menggunakan selector document.getElementsByClassName
+document.getElementsByClassName("kata");
 // 3. dengan menggunakan selector document.querySelector
-document.querySelector("span#kata");
-// 3. dengan menggunakan selector document.querySelectorAll
-document.querySelectorAll("span#kata");
+document.querySelector("span.kata");
+// 4. dengan menggunakan selector document.querySelectorAll
+document.querySelectorAll("span.kata");
