5bc5178b9927926de8a48f169ca535dabd7d549e
grafisaholic <khabiburrokhman13@gmail.com>
2021-10-20 22:23:02 +0700

    feat: Menambahkan materi array manipulation

---
+++ learn/basic/010_array_manipulation/sort.js
@@ -0,0 +1,27 @@
+function sortAscending(point) {
+  let result = point.sort(function (a, b) {
+    return a - b;
+  });
+
+  return result;
+}
+
+function sortDescending(point) {
+  let result = point.sort(function (a, b) {
+    return b - a;
+  });
+
+  return result;
+}
+
+const arrayPoint = [40, 100, 1, 5, 25, 10];
+
+console.log(sortAscending(arrayPoint)); // [ 1, 5, 10, 25, 40, 100 ]
+console.log(sortDescending(arrayPoint)); // [ 100, 40, 25, 10, 5, 1 ]
+
+/**
+ * PENJELASAN :
+ * SORT dalam bahasa indonesia berarty mengurutkan
+ * sortAscending bertujuan untuk mengurutkan angka dalam array menjadi urutan dari yang terkecil ke terbesar
+ * sortDescending bertujuan untuk mengurutkan angka dalam array menjadi urutan dari yang terbesar ke terkecil
+ */
