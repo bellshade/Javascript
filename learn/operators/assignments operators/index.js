b46f15d872df47bbd25dc05bf69c2458df174c26
afifsauqil <afifsauqil18@gmail.com>
2021-10-24 12:00:33 +0700

    feat: add bitwise, comparison, and predence operators

--- learn/operators/assignments operators/index.js
+++
@@ -1,6 +0,0 @@
-let x = 50;
-
-x = x + 5;
-
-x += 5;
-console.log(x);
--- learn/operators/logical operators with non boolean/index.js
+++
@@ -1,9 +0,0 @@
-// and
-console.log(true && true);
-console.log(true && false);
-// or
-console.log(true || true);
-console.log(true || false);
-// not
-console.log(!true);
-console.log(!false);
