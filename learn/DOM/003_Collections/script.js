20c1044b7cbc4d96a78310f7b9b38659ab2b2f9e
KimmyKx <87674246+KimmyKx@users.noreply.github.com>
2021-10-25 15:57:39 +0800

    feat: menambahkan titik koma dan double quote

--- learn/DOM/003_Collections/script.js
+++ learn/DOM/003_Collections/script.js
@@ -1,16 +1,16 @@
 // Deklarasikan variable untuk menyimpan HTML Collections
-const rotasi = document.getElementsByClassName('rotasi')
-const condong = document.getElementsByClassName('condong')
+const rotasi = document.getElementsByClassName("rotasi");
+const condong = document.getElementsByClassName("condong");
 
 // Mengubah rotasi 45 derajat untuk
 // seluruh elemen dengan class rotasi 
 for (let i = 0; i < rotasi.length; i++) {
-    rotasi[i].style.transform = 'rotate(45deg)'
+    rotasi[i].style.transform = "rotate(45deg)";
 }
 
 
 // Mencondongkan elemen 20 derajat-x dan 15 derajat-y untuk
 // seluruh elemen dengan class condog 
 for (let i = 0; i < condong.length; i++) {
-    condong[i].style.transform = 'skew(20deg, 15deg)'
+    condong[i].style.transform = "skew(20deg, 15deg)";
 }
