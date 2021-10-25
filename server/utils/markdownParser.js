09cf8f31abc7138997e89f85d981b5ef85b617f4
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2021-10-25 14:20:08 +0700

    fix: fix codacy issue

--- server/utils/markdownParser.js
+++ server/utils/markdownParser.js
@@ -1,11 +1,11 @@
 const fs = require("fs");
 const commonmark = require("commonmark");
 
-const Parser = (path) => {
+const Parser = (filePath) => {
   const reader = new commonmark.Parser();
   const writer = new commonmark.HtmlRenderer();
 
-  const markdown = fs.readFileSync(path, "utf8");
+  const markdown = fs.readFileSync(filePath, "utf8");
 
   const parsed = reader.parse(markdown);
   return writer.render(parsed);
