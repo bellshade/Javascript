b3871bc9923c2b76f1c63a2cd7e087c293bbf054
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2021-10-26 16:12:43 +0700

    fix: memperbaiki codacy dan menghapus fitur naik satu folder ke atas

---
+++ server/controllers/commonFolder.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const fs = require("fs");
+
+const markdownParser = require("../utils/markdownParser");
+const { ROOT, statics } = require("../config/constant");
+
+const whatIcon = (file) => {
+  const extension = file.split(".")[1];
+
+  switch (extension) {
+    case "html":
+      return "fab fa-html5";
+    case "js":
+      return "fab fa-js";
+    case "md":
+      return "fab fa-md";
+  }
+};
+
+function commonFolder(req, res, originalURL) {
+  const md = markdownParser(path.join(originalURL, "README.md"));
+
+  const items = fs
+    .readdirSync(path.join(ROOT, originalURL))
+    .filter((item) => item !== "README.md")
+    .map((item) => ({
+      name: item,
+      type: !item.includes(".") ? "fas fa-folder" : whatIcon(item)
+    }));
+
+  res.render("common", { md, originalURL, items });
+}
+
+module.exports = commonFolder;
--- server/controllers/main.js
+++ server/controllers/main.js
@@ -4,6 +4,8 @@ const fs = require("fs");
 const markdownParser = require("../utils/markdownParser");
 const { ROOT, commonRegex, statics } = require("../config/constant");
 
+const commonFolder = require("./commonFolder");
+
 function main(req, res) {
   const originalURL = req.originalUrl;
 
@@ -19,36 +21,6 @@ function main(req, res) {
   } else {
     res.status(404).send("NOT FOUND");
   }
-}
-
-const whatIcon = (file) => {
-  const extension = file.split(".")[1];
-
-  switch (extension) {
-    case "html":
-      return "fab fa-html5";
-    case "js":
-      return "fab fa-js";
-    case "md":
-      return "fab fa-md";
-  }
-};
-
-function commonFolder(req, res, originalURL) {
-  const md = markdownParser(path.join(originalURL, "README.md"));
-  const upOneDirectory = statics.includes(originalURL.replace("/"))
-    ? `${originalURL}/..`
-    : "..";
-
-  const items = fs
-    .readdirSync(path.join(ROOT, originalURL))
-    .filter((item) => item !== "README.md")
-    .map((item) => ({
-      name: item,
-      type: !item.includes(".") ? "fas fa-folder" : whatIcon(item)
-    }));
-
-  res.render("common", { md, originalURL, items, upOneDirectory });
 }
 
 module.exports = main;
