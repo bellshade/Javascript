693f8edd5762a2e4c1be7fab59de6b016371007b
hasheemi-rafsanjani <hasemiandika78@gmail.com>
2021-10-26 10:57:19 +0700

    docs: menambahkan materi GeoLocation

---
+++ learn/Web_Api/GeoLocation/geolocation.js
@@ -0,0 +1,21 @@
+let container = document.getElementById("geo-data"); // menyeleksi container
+
+function showGeo() {
+    navigator.geolocation.getCurrentPosition(show); // menggunakan geolocation web api
+}
+function show(position){
+    container.innerHTML = `
+    <div>longitude: ${position.coords.longitude}</div>
+    <br/>
+    <div>latitude: ${position.coords.latitude}</div>
+    `; // mengisi data di container
+    var mymap = L.map('geo-map').setView([-5, 120], 4); // initialisasi leaflet map
+    
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(mymap);
+    
+    L.marker([position.coords.latitude,position.coords.longitude]).addTo(mymap) // menandai lokasi dengan marker
+    .bindPopup(`<div>lokasi mu disini: <br>latitude: ${position.coords.latitude} <br>longitude: ${position.coords.longitude}</div>`); // mengisi data di pop-up 
+    
+}
