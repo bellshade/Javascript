4c07e7ec4e551915de41af0c279a4ad3c38dfba9
hasheemi-rafsanjani <hasemiandika78@gmail.com>
2021-10-27 10:21:33 +0700

    fix: menyesuaikan update

---
+++ learn/Web_Api/GeoLocation/geolocation.js
@@ -0,0 +1,25 @@
+/*eslint-disable no-undef */
+
+let long = document.getElementById("geo-long"); // menyeleksi container longitude
+let lat = document.getElementById("geo-lat"); // menyeleksi container latitude
+let btn = document.getElementById("geo-btn"); // menyeleksi button
+
+function show(position){
+    let longText = document.createTextNode(position.coords.longitude);
+    let latText = document.createTextNode(position.coords.latitude);
+    long.appendChild(longText); // mengisi data longitude
+    lat.appendChild(latText); // mengisi data latitude
+    var mymap = L.map("geo-map").setView([-5, 120], 4); // initialisasi leaflet map
+    
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution: "&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors"
+}).addTo(mymap);
+    
+    L.marker([position.coords.latitude,position.coords.longitude]).addTo(mymap) // menandai lokasi dengan marker
+    .bindPopup(`<div>lokasi mu disini: <br>latitude: ${position.coords.latitude} <br>longitude: ${position.coords.longitude}</div>`); // mengisi data di pop-up 
+    
+}
+function showGeoData() {
+    navigator.geolocation.getCurrentPosition(show); // menggunakan geolocation web api
+}
+btn.addEventListener("click",showGeoData);
