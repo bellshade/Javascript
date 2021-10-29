6eebbc9f7181f98ee323e87fc73de5bc5779ce20
hasheemi-rafsanjani <hasemiandika78@gmail.com>
2021-10-29 16:43:50 +0700

    fix: Merge branch 'bellshade:main' into primeArr

--- algorithm/search/searchPrimeNumber.js
+++ algorithm/search/searchPrimeNumber.js
@@ -2,7 +2,7 @@
  * Dapatkan Bilangan Prima Dalam Range Tertentu
  */
 function isPrime(number) { // mengecek apakah itu bilangan prima
-    
+
   //Mengecek apakah bilangan tersebut lebih kecil dari 2 atau tidak ,karena Bilangan Prima dimulai dari 2, ini juga berfungsi sebagai pengecekan Bilangan negatif
   if (number < 2) {
     return false;
@@ -14,20 +14,17 @@ function isPrime(number) { // mengecek apakah itu bilangan prima
       return false;
     }
   }
-  //Jika perulangan/kondisi diatas tidak memenuhi maka akan return "Bilangan Prima"
+  //Jika perulangan/kondisi diatas tidak memenuhi maka akan return true
   return true;
 }
-function searchPrime(awal,akhir) {
-    let arr = []; // tahap inisialisasi array pembungkus, yang nanti akan berisi hasil bilangan prima
 
-    for (let i = awal; i <= akhir; i++) { // looping mulai dari angka input awal sampai input akhir
-
-        if(isPrime(i) === true){ // cek apakah angka termasuk bilangan prima
-
-            arr.push(i); // jika ya tambahkan angka pada array pembungkus
-        }
-        
+function searchPrime(awal, akhir) {
+  let arr = []; // tahap inisialisasi array pembungkus, yang nanti akan berisi hasil bilangan prima
+  for (let i = awal; i <= akhir; i++) { // looping mulai dari angka input awal sampai input akhir
+    if (isPrime(i) === true) { // cek apakah angka termasuk bilangan prima
+      arr.push(i); // jika ya tambahkan angka pada array pembungkus
     }
-    return arr; // kembalikan dalam bentuk array
+  }
+  return arr; // kembalikan dalam bentuk array
 }
-console.log(searchPrime(7,15)); // output [ 7, 11, 13 ]
+console.log(searchPrime(7, 15)); // output [ 7, 11, 13 ]
