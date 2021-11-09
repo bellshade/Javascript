0270b7cee65b500c9c69437fd47a53500226d3aa
azqilana <mazqulunu@gmail.com>
2021-11-09 23:20:25 +0800

    feat: tambah isi file js

--- learn/DOM/008_Event_Bubbling_and_Capturing/script.js
+++ learn/DOM/008_Event_Bubbling_and_Capturing/script.js
@@ -21,13 +21,29 @@ const anbu = document.getElementsByClassName("anbub1")[0];
 const anbu2 = document.getElementsByClassName("anbub2")[0];
 const anbu3 = document.getElementsByClassName("anbub3")[0];
 
-anbu.addEventListener("click", function () {
-  anbu.classList.toggle("abu");
+anbu.addEventListener("click", function (e) {
+  e.target.classList.toggle("abu");
 });
-anbu2.addEventListener("click", function () {
-  anbu2.classList.toggle("hijau");
+anbu2.addEventListener("click", function (e) {
+  e.target.classList.toggle("hijau");
+  e.stopPropagation();
 });
 anbu3.addEventListener("click", (e) => {
-  anbu3.classList.toggle("kuning");
+  e.target.classList.toggle("kuning");
   e.stopPropagation();
+});
+
+const cap = document.getElementsByClassName("cap1")[0];
+const cap2 = document.getElementsByClassName("cap2")[0];
+const cap3 = document.getElementsByClassName("cap3")[0];
+
+cap.addEventListener("click", function (e) {
+  e.target.classList.toggle("abu");
+});
+cap2.addEventListener("click", () => {
+    cap2.classList.toggle("hijau");
+  });
+cap3.addEventListener("click", (e) => {
+  cap3.classList.toggle("kuning");
+  
 });
