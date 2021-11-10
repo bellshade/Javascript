b31ba9f0a329a7a0986d0faad0fea13b2964d995
azqilana <mazqulunu@gmail.com>
2021-11-10 18:00:49 +0800

    feat: tambah isi file script js

--- learn/DOM/008_Event_Bubbling_and_Capturing/script.js
+++ learn/DOM/008_Event_Bubbling_and_Capturing/script.js
@@ -5,7 +5,7 @@ const bub = contoh.getElementsByClassName("bub")[0];
 const bu1 = bub.getElementsByClassName("bub1")[0];
 const bu2 = bu1.getElementsByClassName("bub2")[0];
 const bu3 = bu2.getElementsByClassName("bub3")[0];
-
+//Event Bubbling berlaku
 bu1.addEventListener("click", function () {
   bu1.classList.toggle("abu");
 });
@@ -16,7 +16,7 @@ bu2.addEventListener("click", function () {
 bu3.addEventListener("click", function () {
   bu3.classList.toggle("kuning");
 });
-
+//Event Bubbling tidak berlaku karean di berikan e.stopPropagation()
 const anbu = document.getElementsByClassName("anbub1")[0];
 const anbu2 = document.getElementsByClassName("anbub2")[0];
 const anbu3 = document.getElementsByClassName("anbub3")[0];
@@ -32,18 +32,18 @@ anbu3.addEventListener("click", (e) => {
   e.target.classList.toggle("kuning");
   e.stopPropagation();
 });
-
+// Event Capturing
 const cap = document.getElementsByClassName("cap1")[0];
 const cap2 = document.getElementsByClassName("cap2")[0];
 const cap3 = document.getElementsByClassName("cap3")[0];
 
-cap.addEventListener("click", function (e) {
-  e.target.classList.toggle("abu");
-});
+cap.addEventListener("click", function () {
+  cap.classList.toggle("abu")
+}, capture = true );
 cap2.addEventListener("click", () => {
-    cap2.classList.toggle("hijau");
-  });
-cap3.addEventListener("click", (e) => {
-  cap3.classList.toggle("kuning");
-  
+  cap2.classList.toggle("hijau");
 });
+cap3.addEventListener("click", function () {
+  cap3.classList.toggle("kuning");
+});
+
