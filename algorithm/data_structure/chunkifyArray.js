5c9d9cf6e2f4066e7aa7ab0aa730caa4e70fba8b
Ezra Khairan Permana <48118327+reacto11mecha@users.noreply.github.com>
2021-12-10 07:59:33 +0700

    Menambahkan socket io sebagai dependensi dan menjalankan prettier (#241)
    * feat: menambahkan socket io
    
    * fix: menjalankan prettier

--- algorithm/data_structure/chunkifyArray.js
+++ algorithm/data_structure/chunkifyArray.js
@@ -1,43 +1,32 @@
-const dataList = [
-    'a',
-    'b',
-    'c',
-    'd',
-    'e',
-    '1',
-    '2',
-    '3',
-    '4',
-    '5'
-];
+const dataList = ["a", "b", "c", "d", "e", "1", "2", "3", "4", "5"];
 
 /**
- * @param { Array[*] } arr 
- * @param { Integer } splitInto 
+ * @param { Array[*] } arr
+ * @param { Integer } splitInto
  * @returns { Array[Array[*]] }
  */
 function chunkifyArray(arr, splitInto) {
-    if(!Number.isInteger(splitInto)) {
-         throw new Error("Array chunk must be divided by an integer number!");
+  if (!Number.isInteger(splitInto)) {
+    throw new Error("Array chunk must be divided by an integer number!");
+  }
+  let returnArr = [];
+  let temp = [];
+  for (let i = 0; i < arr.length; i++) {
+    temp.push(arr[i]);
+    if ((i + 1) % splitInto === 0 || i + 1 === arr.length) {
+      returnArr = [...returnArr, temp];
+      temp = [];
     }
-    let returnArr = [];
-    let temp = []; 
-    for(let i = 0; i < arr.length; i++) {
-        temp.push(arr[i]);
-        if((i+1) % splitInto === 0 || i+1 === arr.length) {
-            returnArr = [...returnArr, temp];
-            temp = [];
-        }
-    }
-    return returnArr;
+  }
+  return returnArr;
 }
 
 /**
  * Penjelasan Kode:
- * 
+ *
  * function chunkifyArray akan membagi array yang di pass ke parameter arr
  * dan akan coba membagi rata berdasarkan parameter splitInto
-*/
+ */
 
 console.log(chunkifyArray(dataList, 3));
 // >> [ ['a', 'b', 'c'], ['d', 'e', '1'], ['2', '3', '4'], ['5'] ]
