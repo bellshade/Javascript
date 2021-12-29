4bde2381dca777c4d9fb3a38454fb9d6506bee35
Ezra Khairan Permana <48118327+reacto11mecha@users.noreply.github.com>
2021-12-29 14:12:04 +0700

    fix: memperbaiki navigasi badge yang salah (#251)
    * fix: memperbaiki navigasi badge yang salah
    
    * docs: memperbaiki indentasi komentar kode

--- server/utils/markdownParser.js
+++ server/utils/markdownParser.js
@@ -12,6 +12,21 @@ const { ROOT } = require("../config/constant");
 const imageSrcRegex = /img src="(?!http(s?))/g;
 const emojiReplacer = (match) => emoji.emojify(match);
 
+/*
+ * Replacer tombol navigasi, valid ketika anchor berisi image
+ * yang srcnya merujuk ke badge maker api bellshade
+ *
+ * Regex dasarnya seperti ini
+ * new RegExp("(<a href=(.*)>)(<img align=(.*) src=(\"https://api.bellshade.org/badge/.*\") />)(</a>)")
+ *
+ * Yang pertama berupa regex global untuk mencari,
+ * Yang kedua berupa regex yang memudahkan untuk mengganti value
+ */
+const navigationSearcher =
+  /(<a href=(.*)>)(<img align=(.*) src=("https:\/\/api.bellshade.org\/badge\/.*") \/>)(<\/a>)/g;
+const navigationReplacer =
+  /(<a href=(.*)>)(<img align=(.*) src=("https:\/\/api.bellshade.org\/badge\/.*") \/>)(<\/a>)/;
+
 const Parser = (readmePath, originalURL) => {
   const normalize = path.normalize(readmePath);
   const fullPath = path.join(ROOT, normalize);
@@ -24,7 +39,7 @@ const Parser = (readmePath, originalURL) => {
 
   const result = marked.parse(markdown);
 
-  const replaceAll = result
+  let replaceAll = result
     // replace CONTRIBUTING.md untuk di arahkan ke github
     .replace(
       "CONTRIBUTING.md",
@@ -34,6 +49,35 @@ const Parser = (readmePath, originalURL) => {
     .replace(imageSrcRegex, `img src="${path.join("/static", originalURL)}/`)
     // Replace emoji github, diubah ke unicode
     .replace(/(:.*:)/g, emojiReplacer);
+
+  // Mencari semua tombol navigasi yang ada, jika ada lakukan perulangan
+  const searchMatchNavigationButton = replaceAll.match(navigationSearcher);
+  if (Array.isArray(searchMatchNavigationButton)) {
+    searchMatchNavigationButton.forEach((button) => {
+      // Mengcopy value dari tombol yang ada
+      const copyButton = [...button].join("");
+
+      // Rematch ulang tombol yang dapat perulangan navigasi yang valid
+      /* Keterangan reMatch
+       * index 0: Valuenya sama seperti button
+       * index 1: Valuenya hanya berupa <a href="{navigasi yang salah}">
+       * index 2: Valuenya hanya berupa path navigasi yang salah, misal
+       * "../012_string_includes"
+       */
+      const reMatch = button.match(navigationReplacer);
+
+      // Mendapatkan path yang seharusnya
+      const realPath = path.posix.normalize(
+        `${originalURL}/${reMatch[2].replace(/"/g, "")}`
+      );
+
+      // Mengganti href yang salah
+      const replaced = copyButton.replace(reMatch[1], `<a href="${realPath}">`);
+
+      // Assign ulang tombol navigasi yang diganti
+      replaceAll = replaceAll.replace(button, replaced);
+    });
+  }
 
   return replaceAll;
 };
