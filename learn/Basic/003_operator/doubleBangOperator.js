5a72fb39dfe2e2e13dc3a4d7a9390df2a14f6a0a
KimmyKx <kimmykondoy5@gmail.com>
2022-01-24 21:53:27 +0800

    docs: menambahkan operator double bang

---
+++ learn/Basic/003_operator/doubleBangOperator.js
@@ -0,0 +1,31 @@
+/**
+ * Double Bang (!!)
+ * 
+ * Double Bang merupakan operator yang mengkonversi suatu value
+ * menjadi tipe data boolean, perlu di ingat operator ini bukan
+ * berarti menggunakan operator NOT (!) sebanyak 2 kali
+ * operator ini sama saja dengan menggunakan fungsi Boolean()
+ * yang JavaScript miliki hanya saja lebih sederhana.
+ * 
+ * Referensi: https://dev.to/sanchithasr/what-is-the-double-bang-operator-in-javascript-4i3h
+ * 
+ * Berikut contoh value yang dianggap truthy oleh JavaScript:
+ */
+
+const obj = {};
+const arr = [];
+const bukanStringKosong = "abc";
+const angkaSelainNol = 2.5;
+const date = new Date();
+const truthy = true;
+console.log(!!obj, !!arr); // true true
+// Diatas merupakan data yang akan dianggap true
+
+// Contoh value yang akan dianggap false:
+const kosong = "";
+const nol = 0;
+const nullish = null;
+const notDefined = undefined;
+const bukanAngka = NaN;
+const falsey = false;
+console.log(!!kosong, !!nol); // false false
