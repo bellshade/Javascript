fa45fc27b1ed3d9827f88ab68448f32486959c93
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2022-03-17 14:16:48 +0700

    feat: menambahkan `@bellshade/ningali` sebagai ganti folder `server`

--- server/utils/index.js
+++
@@ -1,5 +0,0 @@
-const checker = require("./checker");
-const markdownParser = require("./markdownParser");
-const directoryScanner = require("./directoryScanner");
-
-module.exports = { checker, markdownParser, directoryScanner };
---
+++ viewer.js
@@ -0,0 +1,8 @@
+const ningali = require("@bellshade/ningali");
+
+const app = ningali({
+  folders: ["learn", "algorithm"],
+  title: "Bellshade Javascript",
+});
+
+app();
