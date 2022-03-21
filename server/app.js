821aa05b19591b61dbca6592b74fcf54856fce55
Lintang S. Pinasih <92625668+LintangDev@users.noreply.github.com>
2022-03-21 13:33:41 +0700

    feat: menambahkan sistem ganti port pada webserver (#292)
    * feat: menambahkan main pada package.json
    
    * feat: menambahkan sistem ganti port pada webserver
    
    * docs: menambahkan readme pada learn/basic
    
    * feat: mengganti sistem port sesuai yang di minta
    
    * docs: mengubah sedikit readme
    
    * docs: menghapus dokumentasi yang ditambahkan sebelumnya
    
    * feat: fix issue codacy
    
    * feat: pin dependency

--- server/app.js
+++ server/app.js
@@ -1,17 +1,19 @@
+require("dotenv").config();
 const fastify = require("fastify");
 const path = require("path");
 
 const autoLoad = require("fastify-autoload");
 
 const app = fastify({ debug: false });
+const port = process.env.PORT || 3000;
 
 app.register(autoLoad, {
   dir: path.join(__dirname, "plugins")
 });
 
 const start = async () => {
-  await app.listen(3000);
-  console.log("Listening on port 3000 | http://localhost:3000/");
+  await app.listen(port);
+  console.log(`Listening on port ${port} | http://localhost:${port}/`);
 };
 
 start();
