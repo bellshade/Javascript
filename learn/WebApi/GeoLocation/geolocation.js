49aaa76ca489b4b26c2309c7f76eb6100aa5bb59
arcetros <75669794+arcetros@users.noreply.github.com>
2022-07-28 02:29:56 +0800

    feat: add geolocation status

--- learn/WebApi/GeoLocation/geolocation.js
+++ learn/WebApi/GeoLocation/geolocation.js
@@ -3,12 +3,15 @@
 let long = document.getElementById("geo-long"); // menyeleksi container longitude
 let lat = document.getElementById("geo-lat"); // menyeleksi container latitude
 let btn = document.getElementById("geo-btn"); // menyeleksi button
+let navStatus = document.getElementById("status"); // menyeleksi container
 
 function show(position) {
   const longText = document.createTextNode(position.coords.longitude);
   const latText = document.createTextNode(position.coords.latitude);
   long.appendChild(longText); // mengisi data longitude
   lat.appendChild(latText); // mengisi data latitude
+  navStatus.innerHTML = "Access Granted";
+  navStatus.style.color = "green";
   let mymap = L.map("geo-map").setView([-5, 120], 4); // initialisasi leaflet map
 
   L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
@@ -22,7 +25,13 @@ function show(position) {
       `<div>lokasi mu disini: <br>latitude: ${position.coords.latitude} <br>longitude: ${position.coords.longitude}</div>`
     ); // mengisi data di pop-up
 }
+
+function error(err) {
+  navStatus.innerHTML = `ERROR: ${err.message} (${err.code})`;
+  navStatus.style.color = "red";
+}
+
 function showGeoData() {
-  navigator.geolocation.getCurrentPosition(show); // menggunakan geolocation web api
+  navigator.geolocation.getCurrentPosition(show, error);
 }
 btn.addEventListener("click", showGeoData); // menjalankan showGeoData saat btn diklik
