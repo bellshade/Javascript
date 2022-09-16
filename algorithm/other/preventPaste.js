f6b7e2efd5e20e3743e26bb4e605f291362faf02
Mushlih Almubarak <mushlih_31@mushlih.cf>
2022-09-16 11:15:26 +0300

    docs: menambahkan contoh penggunakan fungsi preventPaste

--- algorithm/other/preventPaste.js
+++ algorithm/other/preventPaste.js
@@ -1,7 +1,10 @@
 /**
  * Fungsi untuk melarang user mem-paste di inputan form website kita.
  * Fungsi ini akan mengembalikan alert javascript "Dilarang mem-paste!" jika user melakukan paste pada sebuah inputan form.
- * Silakan jalankan fungsi dibawah ini menggunakan atribut onpaste="preventPaste(event)" di dalam tag <input>.
+ * Jalankan fungsi dibawah ini menggunakan atribut onpaste="preventPaste(event)" di dalam tag <input>.
+ * @example
+ * // Elemen input di bawah ini tidak akan bisa dipaste oleh user.
+ * <input type="text" onpaste="preventPaste(event)"/>
  * @param {object} - Berisi even yang sedang dijalankan
  */
 
