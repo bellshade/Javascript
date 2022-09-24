6830916e0632a6e68e34e02e75a672542eed3899
Mushlih Almubarak <mushlih_31@mushlih.cf>
2022-09-24 10:35:22 +0300

    style: menambah jarak di fungsi preventPaste (https://github.com/bellshade/Javascript/pull/339)

--- algorithm/WebRelated/preventPaste/prevent.js
+++ algorithm/WebRelated/preventPaste/prevent.js
@@ -5,6 +5,7 @@
 
 // Mengambil elemen input yang akan dilarang mem-paste.
 const input = document.querySelector("input");
+
 // Membuat event listener jika user melakukan paste pada inputan form.
 input.addEventListener("paste", function (e) {
     e.preventDefault(); // Mencegah agar tidak bisa dipaste
