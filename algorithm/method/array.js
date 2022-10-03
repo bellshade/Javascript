cee9b8ccbde1e01fd50d784c6e9796bdb96e13f4
Ricky J <rickyjulpiter14@gmail.com>
2022-10-03 17:29:23 +0700

    feat: menambahkan materi built-in function array

--- algorithm/method/array.js
+++ algorithm/method/array.js
@@ -2,9 +2,9 @@
 Javascript menyediakan built-in function yang dapat kita gunakan pada array
 */
 
-const exampleArray = [4,5,6,7];
+const exampleArray = [4, 5, 6, 7];
 
-// Menemukan value pada array pada indeks tertentu 
+// Menemukan value pada array pada indeks tertentu
 const at = exampleArray.at(1); // 5
 
 // Memasukkan value ke dalam array
@@ -20,16 +20,16 @@ const reverse = exampleArray.reverse(); // [7, 6, 5, 4]
 const includes = exampleArray.includes(6); // true
 
 // Melakukan perulangan pada array secara langsung terhadap nilainya
-const map = exampleArray.map(item => item * 2); // [8, 10, 12, 14]
+const map = exampleArray.map((item) => item * 2); // [8, 10, 12, 14]
 
 // Filter array value dengan kondisi tertentu
-const filter = exampleArray.filter(item => item > 5); // [6, 7]
+const filter = exampleArray.filter((item) => item > 5); // [6, 7]
 
 // Find array value dengan kondisi tertentu
-const find = exampleArray.find(item => item > 5); // 6 // first match
+const find = exampleArray.find((item) => item > 5); // 6 // first match
 
 // Melakukan pengecekan pada setiap value array dengan kondisi tertentu
-const every = exampleArray.every(item => item > 0); // true
+const every = exampleArray.every((item) => item > 0); // true
 
 // Mencari index dari sebuah value di dalam array
-const findIndex = exampleArray.findIndex(item => item === 5); // 1
+const findIndex = exampleArray.findIndex((item) => item === 5); // 1
