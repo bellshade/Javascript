2f27d3131aec92d84ab6018f3e8ee0be357703ec
Mushlih Almubarak <mushlih_31@mushlih.cf>
2022-10-05 19:10:05 +0300

    style: menghubungkan/menggabungkan file JavaScript dengan file HTML/demonya

--- algorithm/WebRelated/preventCopy/prevent.js
+++ algorithm/WebRelated/preventCopy/prevent.js
@@ -3,10 +3,16 @@
  * Fungsi ini akan mengembalikan alert javascript "Dilarang menyalin tulisan ini!" jika user melakukan copy pada tulisan di website kita.
  */
 
-// Mengambil elemen yang tidak boleh di-copy.
+// Mengambil elemen-elemen yang tidak boleh di-copy.
+const heading = document.querySelector("h1");
 const paragraph = document.querySelector("p");
 
 // Membuat event listener jika user melakukan copy pada elemen yang kita inginkan agar tidak bisa di-copy.
+heading.addEventListener("copy", function (e) {
+    e.preventDefault(); // Mencegah agar tidak bisa di-copy
+    alert("Dilarang menyalin tulisan ini!"); // Alert ditampilkan, teks bisa disesuaikan
+});
+
 paragraph.addEventListener("copy", function (e) {
     e.preventDefault(); // Mencegah agar tidak bisa di-copy
     alert("Dilarang menyalin tulisan ini!"); // Alert ditampilkan, teks bisa disesuaikan
