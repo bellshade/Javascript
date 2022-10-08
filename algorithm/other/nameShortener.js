6405bf6aae37bef71c22e0a224ee18d7a9dabb93
onixldlc <onxdl16427@outlook.com>
2022-10-08 23:29:55 +0700

    feat: menambahkan algoritma penyingkatan nama

---
+++ algorithm/other/nameShortener.js
@@ -0,0 +1,62 @@
+function nameShortner(fullname, maxLen = 24, capitalize = 1) {
+  // ------------------------ helper functions ------------------------
+  function nameLen(arr) {
+    return arr.join(" ").length;
+  }
+  function init(fullname) {
+    return fullname
+      .split(", ")[0]
+      .split(" ") // split string nya agar bisa di manipulasi
+      .reverse(); // reverse array nya agar bisa mulai dari belakang
+  }
+  function capitalized(name) {
+    return name[0].toUpperCase() + name.substring(1); // ambil karakter pertama ubah jadi kapital dan tambahkan sisa nya
+  }
+  function singleName(shorten) {
+    while (true) {
+      shorten[0].trim();
+      if (nameLen(shorten) <= maxLen) break; // hilangkan semua singkatan hingga karakter nya sesuai
+      if (shorten.length < 2) break; // bila nama memang tersisa satu maka keluar dan print
+      shorten.shift();
+    }
+  }
+  // ------------------------ helper functions ------------------------
+
+  var shorten = init(fullname);
+  var currentLen = fullname.length;
+
+  for (var [i, name] of shorten.entries()) {
+    // i sebagai index, dan name sebagai nama
+    currentLen = nameLen(shorten); // memperbarui total panjang nama
+    if (capitalize) shorten[i] = capitalized(name); // bila nama butuh huruf kapital di awal tiap nama
+    if (currentLen <= maxLen && name.length > 2) continue; // lewati 1 loop bila total panjang sudah sesuai dan nama belum di singkat
+    if (i != shorten.length - 1) {
+      // bila nama yang belum di singkat masih lebih dari 1
+      shorten[i] = name.substring(0, 1).toUpperCase() + "."; // ambil karakter pertama, lalu di buat menjadi kapital, dan tambah kan .
+    } else {
+      singleName(shorten);
+    }
+  }
+  return shorten.reverse().join(" "); // reverse array nya ke posisi semula dan gabungkan
+}
+
+console.log(nameShortner("muhammad jaka wiliam kent"));
+// Muhammad Jaka Wiliam K.
+
+console.log(nameShortner("juliyani zubaeda M"));
+// Juliyani Zubaeda M.
+
+console.log(nameShortner("james k widjaya"));
+// Jamews K. Widjaya
+
+console.log(nameShortner("james k widjaya", 15));
+// Jamews K.
+
+// Note Bugs:
+//  1. algoritma ini tidak dapat membedakan gelar yang berada di depan nama
+console.log(nameShortner("Ir. Mulawanmar", 10));
+// Ir. M.
+
+//  2. algoritma ini akan menghilangkan gelar di belakang nama, dikarenakan penulisan yang terkadang tidak konsisten
+console.log(nameShortner("nicholas bambang prasetyo, S.Pd., M.Pd."));
+// Nicholas Bambang P.
