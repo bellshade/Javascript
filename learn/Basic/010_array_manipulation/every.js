8bb74172aa9bbe4c2e961c39387ceed74ad9f519
Ricky J <rickyjulpiter14@gmail.com>
2022-10-11 14:25:04 +0700

    add: array manipulation - every

---
+++ learn/Basic/010_array_manipulation/every.js
@@ -0,0 +1,6 @@
+const exampleArray = [60, 70, 80, 90];
+
+console.log(exampleArray.every((value) => value > 50)); // output: true
+
+// every() berfungsi untuk mengecek apakah setiap nilai pada array lulus pada pengujian/fungsi yang di terapkan.
+// hasil dari every() yaitu true atau false
