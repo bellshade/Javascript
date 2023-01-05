4d98fbb13365b45e7f51fec64879f78edbd916a2
azhardanii <azhardanii@yahoo.com>
2023-01-06 02:45:07 +0700

    docs: Menambahkan Dokumentasi Function Scope dan Keyword This

---
+++ learn/Basic/008_function/functionScope.js
@@ -0,0 +1,37 @@
+// eslint-disable no-undef
+// == Contoh == 
+
+// 1. Mendefisikan variabel didalam fungsi
+function memancing() {
+  let ikan = "Lele";
+  console.log(ikan);
+}
+
+// Menampilkan variabel ikan yang didefinisikan didalam fungsi memancing()
+console.log(ikan); // Error: ikan tidak terdefinisikan
+memancing(); // Lele
+
+// 2. Mendefisikan variabel diluar fungsi
+let ikan = "Gurame";
+function kolamIkan() {
+  console.log(ikan);
+}
+
+// Menampilkan variabel ikan yang didefinisikan diluar fungsi kolamIkan()
+console.log(ikan); // Gurame
+kolamIkan(); // Gurame
+
+// 3. Deklarasi variabel didalam dan diluar fungsi
+let noktunal = "Kelelawar";
+let diurnal = "Gajah";
+
+function hewan() {
+  let nokturnal = "Burung Hantu";
+  console.log(nokturnal);
+  console.log(diurnal);
+}
+
+// Menampilkan variabel mamalia yang didefinisikan didalam dan diluar fungsi
+console.log(diurnal); // Gajah
+console.log(noktunal); // Kelelawar
+hewan(); // Burung Hantu & // Gajah
