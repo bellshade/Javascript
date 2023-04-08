b0d4c3dc201498fd41efb75d0a9782c2f023d202
tio <sulistiopratomo@gmail.com>
2023-04-09 06:31:14 +0700

    fix: revisi contoh kode

--- learn/Basic/015_nullish_coalescing_operator/example.js
+++ learn/Basic/015_nullish_coalescing_operator/example.js
@@ -1,12 +1,11 @@
 const nullValue = null;
-const emptyText = ""; // falsy
+const emptyText = "";
 const someNumber = 42;
 
 const valA = nullValue ?? "default for A";
 const valB = emptyText ?? "default for B";
 const valC = someNumber ?? null;
 
-
 console.log(valA); // "default for A"
-console.log(valB); // "" (as the empty string is not null or undefined)
+console.log(valB); // ""
 console.log(valC); // 42
