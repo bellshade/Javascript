1d4652ccfa726795301bcbc413982fc17c9d7871
DikDns <dikdnssocial@gmail.com>
2023-08-07 07:47:10 +0700

    add: filterToAlphabetOnly function

--- algorithm/cipher/porta.js
+++ algorithm/cipher/porta.js
@@ -1,27 +1,36 @@
-function getColumn(key) {
-  const charACode = 65;
-  let temp = key.charCodeAt() - charACode;
+function portaCipher(string, keys = "PORTA") {
+  function getColumn(key) {
+    const charACode = 65;
+    let temp = key.charCodeAt() - charACode;
 
-  return temp % 2 !== 0 ? --temp : temp;
-}
+    return temp % 2 !== 0 ? --temp : temp;
+  }
 
-function getCurrentColumnRow(key) {
-  const nToZ = "NOPQRSTUVWXYZ";
-  const column = getColumn(key);
-  const shiftLength = column / 2;
-  const endOfShiftLength = nToZ.length + shiftLength;
+  function getCurrentColumnRow(key) {
+    const nToZ = "NOPQRSTUVWXYZ";
+    const column = getColumn(key);
+    const shiftLength = column / 2;
+    const endOfShiftLength = nToZ.length + shiftLength;
 
-  return (
-    nToZ.substring(shiftLength, endOfShiftLength) +
-    nToZ.substring(0, shiftLength)
-  );
-}
+    return (
+      nToZ.substring(shiftLength, endOfShiftLength) +
+      nToZ.substring(0, shiftLength)
+    );
+  }
 
-function swapCharFromPeriodicTable(key, charToSwap) {
-  const aToMColumn = "ABCDEFGHIJKLM";
-  const keyColumn = getCurrentColumnRow(key);
+  function swapCharFromPeriodicTable(key, charToSwap) {
+    const aToMColumn = "ABCDEFGHIJKLM";
+    const keyColumn = getCurrentColumnRow(key);
 
-  return aToMColumn.includes(charToSwap)
-    ? keyColumn[aToMColumn.indexOf(charToSwap)]
-    : aToMColumn[keyColumn.indexOf(charToSwap)];
+    return aToMColumn.includes(charToSwap)
+      ? keyColumn[aToMColumn.indexOf(charToSwap)]
+      : aToMColumn[keyColumn.indexOf(charToSwap)];
+  }
+
+  function filterToAlphabetOnly(string) {
+    return string
+      .toUpperCase()
+      .split(/[\W\d]/)
+      .join("");
+  }
 }
