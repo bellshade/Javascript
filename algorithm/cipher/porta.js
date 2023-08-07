a8da59064e11bfbc3f4e94058dc057a9b2c65808
DikDns <dikdnssocial@gmail.com>
2023-08-07 07:43:44 +0700

    add: getCurrentColumnRow function

--- algorithm/cipher/porta.js
+++ algorithm/cipher/porta.js
@@ -1,3 +1,15 @@
+function getCurrentColumnRow(key) {
+  const nToZ = "NOPQRSTUVWXYZ";
+  const column = getColumn(key);
+  const shiftLength = column / 2;
+  const endOfShiftLength = nToZ.length + shiftLength;
+
+  return (
+    nToZ.substring(shiftLength, endOfShiftLength) +
+    nToZ.substring(0, shiftLength)
+  );
+}
+
 function swapCharFromPeriodicTable(key, charToSwap) {
   const aToMColumn = "ABCDEFGHIJKLM";
   const keyColumn = getCurrentColumnRow(key);
